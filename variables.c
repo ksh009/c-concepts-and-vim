51d587ff8ee7de2190b9baa92d820c20eb221123
HughesSeanKyle <khughessean@yahoo.com>
2023-04-28 03:22:15 +0200

    var assignments and format specifier

---
+++ variables.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main() {
+  // int
+  int myNum = 15;
+
+  // int without assignment 
+  int myNumTwo;
+  myNumTwo = 18;
+
+  // format specifier
+  printf("%d\n", myNum);
+  printf("%d", myNumTwo);
+}
+
+
+
